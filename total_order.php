5e42f05330903039783c536c1f223616d8ae2eab
kaconk uciha <harutonouciha@gmail.com>
2018-02-25 22:16:36 +0700

    tabel merge

--- total_order.php
+++ total_order.php
@@ -28,8 +28,8 @@ if (isset($_POST['submit'])) {
      echo "<p>From ".$tanggal_awal." To ".$tanggal_akhir."</p>";
     $query = "SELECT dept, sum(amount)as total, currency FROM tb_incoming WHERE arrive_date >= '$tanggal_awal' AND arrive_date <= '$tanggal_akhir' group by dept, currency order by dept asc ";
     $result= sqlsrv_query ($conn, $query);
-    echo "<table border ='1' width = '800'>
-    </script>
+    echo "<table border ='1' width = '800' id='tblSample' >
+    <thead>
     <tr>
     <th rowspan='2'>Departemen</th>
     <th colspan='3'>Total</th>
@@ -39,7 +39,8 @@ if (isset($_POST['submit'])) {
     <th>IDR</th>
     <th>USD</th>
     <th>JPY</th>
-    </tr>";
+    </tr>
+    </thead>";
 
     setlocale(LC_MONETARY, 'en_US');
 
@@ -64,6 +65,7 @@ while ($data = sqlsrv_fetch_array($result)){
     $angka = number_format($data['total']);
 
     echo "
+    <tbody>
    <tr>
    
     <td>".$data['dept']."</td>
@@ -71,7 +73,7 @@ while ($data = sqlsrv_fetch_array($result)){
     <td align='center'>$ ".$usd."</td>
     <td align='center'>¥ ".$jpy."</td>
 <td><a href=list_order.php?item=$data[dept] style=text-decoration:none onclick=post>Detail</a></td>
-    </tr>
+    </tr></tbody>
     ";
     //sqlsrv_close();
 }
@@ -87,3 +89,40 @@ while ($data = sqlsrv_fetch_array($result)){
 
 require_once "view/footer.php";
 ?>
+<script type="text/javascript">
+    /*$(document).ready(function () {
+        $('#tblSample').each(function () {
+            var Column_number_to_Merge = 1;
+ 
+            // Previous_TD holds the first instance of same td. Initially first TD=null.
+            var Previous_TD = null;
+            var i = 1;
+            $("tbody",this).find('tr').each(function () {
+                // find the correct td of the correct column
+                // we are considering the table column 1, You can apply on any table column
+                var Current_td = $(this).find('td:nth-child(' + Column_number_to_Merge + ')');
+                 
+                if (Previous_TD == null) {
+                    // for first row
+                    Previous_TD = Current_td;
+                    i = 1;
+                } 
+                else if (Current_td.text() == Previous_TD.text()) {
+                    // the current td is identical to the previous row td
+                    // remove the current td
+                    Current_td.remove();
+                    // increment the rowspan attribute of the first row td instance
+                    //Previous_TD.attr('rowspan', i + 1);
+                    Previous_TD.attr('rowspan', i + 1);
+                    i = i + 1;
+                } 
+                else {
+                    // means new value found in current td. So initialize counter variable i
+                    Previous_TD = Current_td;
+                    i = 1;
+                }
+            });
+        });
+    });
+    */
+</script>
